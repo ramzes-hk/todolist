a91d3fce011d098a47db9ff16efd7aef1c5bf988
Ramzes <68265608+ramzes-hk@users.noreply.github.com>
2023-09-13 16:33:31 -0400

    replaced button with details tag

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, redirect, g, request, flash
+from flask import Flask, g, request, flash
 import sqlite3
 from jinja2 import Environment, FileSystemLoader, select_autoescape, Template
 from pydantic import UUID4, BaseModel, FutureDatetime, Field
@@ -14,7 +14,7 @@ list_length: int = 0
 
 
 DATABASE_PATH = "app.db"
-create = '''
+create = """
     CREATE TABLE IF NOT EXISTS notes (
     id TEXT PRIMARY KEY,
     note TEXT NOT NULL,
@@ -23,27 +23,25 @@ create = '''
     alert_level INTEGER NOT NULL,
     status INTEGER NOT NULL
     );
-'''
+"""
 
 insert = "INSERT INTO notes (id, note, creation_date, deadline, alert_level, status) VALUES (?, ?, ?, ?, ?, ?)"
 
-env = Environment(
-    loader=FileSystemLoader("templates"),
-    autoescape=select_autoescape()
-)
+env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
 
 
 class Note(BaseModel):
     id: UUID4 = uuid.uuid4()
-    note: str = Field(max_length=200) # constr(strip_whitespace=True, max_length=200)  
+    # constr(strip_whitespace=True, max_length=200)
+    note: str = Field(max_length=200)
     creation_date: datetime = datetime.now()
-    deadline: FutureDatetime | None = None  
+    deadline: FutureDatetime | None = None
     alert_level: int = Field(ge=0, le=2, default=2)
     status: int = 0
 
 
 def get_db():
-    if 'db' not in g:
+    if "db" not in g:
         g.db = sqlite3.connect(DATABASE_PATH)
         g.db.execute(create)
         g.db.commit()
@@ -60,26 +58,31 @@ def index():
     return table.render({"items": items}), 200, headers
 
 
-@app.route("/new_note", methods=["GET"])
-def new_note_page():
-    temp = env.get_template("new_note.html")
-    return temp.render()
-
-
 @app.route("/new_note", methods=["POST"])
 def add_note():
     note = Note(id=uuid.uuid4(), note=request.form.get("note", ""))
     db = get_db()
     try:
-        db.execute(insert, (str(note.id), note.note, note.creation_date, note.deadline, note.alert_level, note.status))
+        db.execute(
+            insert,
+            (
+                str(note.id),
+                note.note,
+                note.creation_date,
+                note.deadline,
+                note.alert_level,
+                note.status,
+            ),
+        )
         db.commit()
-    except:
+    except BaseException:
         print("not a unique id: ", note.id)
     flash("New note created successfully")
 
     global list_length
     list_length += 1
     return create_li(list_length, note)
+
 
 @app.route("/note", methods=["POST"])
 def done_note():
@@ -97,17 +100,20 @@ def done_note():
     note = get_note(id)
     return crossover(note, status)
 
+
 def get_status(id: str) -> int:
     db = get_db()
     cursor = db.execute("SELECT status FROM notes WHERE id=(?)", (str(id),))
     status = cursor.fetchone()[0]
-    return status 
+    return status
+
 
 def get_note(id: str) -> str:
     db = get_db()
     cursor = db.execute("SELECT note FROM notes WHERE id=(?)", (str(id),))
     note = cursor.fetchone()[0]
-    return note 
+    return note
+
 
 def crossover(text: str, status: int) -> str:
     if status == 1:
@@ -116,32 +122,30 @@ def crossover(text: str, status: int) -> str:
         templ = Template("{{note}}")
     return templ.render({"note": text})
 
-def create_li(index:int, item: Note) -> str:
+
+def create_li(index: int, item: Note) -> str:
     temp = env.get_template("list_item.html")
     return temp.render({"index": index, "item": item})
+
 
 def get_all() -> list[Note]:
     notes: list[Note] = []
     db = get_db()
     for row in db.execute("SELECT * FROM notes"):
         print(row)
-        notes.append(Note(id=row[0], note=row[1], creation_date=row[2], deadline=row[3], alert_level=row[4], status=row[5]))
-    return notes 
+        notes.append(
+            Note(
+                id=row[0],
+                note=row[1],
+                creation_date=row[2],
+                deadline=row[3],
+                alert_level=row[4],
+                status=row[5],
+            )
+        )
+    return notes
 
-# async def get_note(id: int):
-#     cursor.execute("SELECT * FROM notes WHERE id=(?)", (id,))
-#     data = cursor.fetchone()
-#     note = Note(id=data[0], note=data[1], creation_date=data[2], deadline=data[3], alert_level=data[4])
-#     return note
 
-
-
-# @app.put("/note/{id}")
-# async def update_note(id: int, note: Note):
-#     cursor.execute("UPDATE notes SET note = (?), deadline = (?), alert_level = (?) WHERE id = (?)",
-#                    (note.note, note.deadline, note.alert_level, id))
-#     return note
-#
 @app.route("/note", methods=["DELETE"])
 def delete_note():
     id = request.form.get("id")
@@ -150,4 +154,4 @@ def delete_note():
     db = get_db()
     db.execute("DELETE FROM notes WHERE id=(?)", (id,))
     db.commit()
-    return "", 200 
+    return "", 200
